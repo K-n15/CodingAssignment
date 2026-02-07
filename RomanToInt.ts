a68c2dd76da89a42b9d999a5d479ca51156ab38b
K-n15 <theepapon7055@gmail.com>
2026-02-07 11:04:03 +0700

    - Added more test cases - Build the function to translate RomanToInt - Test run
    Current time 11:05 AM

--- RomanToInt.ts
+++ RomanToInt.ts
@@ -3,12 +3,27 @@ const translate = new Map<string,number>([
 ])
 
 function RomanToInt(numeral : string): number{
-    return 0;
+    let result = 0;
+    const size = numeral.length;
+    for (let i=0 ; i < size-1 ; i++){
+        const curr = numeral.charAt(i);
+        const next = numeral.charAt(i+1);
+        //Only one smaller numeral at most exist before
+        //a bigger numeral, Ex. IIV is not 3 but III is
+        if (translate.get(curr)! < translate.get(next)!){
+            result -= translate.get(curr)!;
+        }else {
+            result += translate.get(curr)!;
+        }
+    }
+    result += translate.get(numeral.charAt(size-1))!;
+    return result;
 }
 
 function testCase(){
     const cases = new Map<string,number>([
-        ['IV',4],['IX',9],['XII',12],['MMXXIV',2024],['MCMLXIV',1964],['LVIII',58],['MCMXCIV',1994],['MCMIII',1903]
+        ['IV',4],['IX',9],['XII',12],['MMXXIV',2024],['MCMLXIV',1964],['LVIII',58],['MCMXCIV',1994],['MCMIII',1903],
+        ['XXXIX',39], ['LXXX',80], ['XVIII',18], ['CMXCIX', 999]
     ]);
     for (const [roman, value] of cases){
         const result = RomanToInt(roman);
@@ -18,7 +33,6 @@ function testCase(){
             throw Error(`${roman} does not translate to ${result}`)
         }
     }
-    console.log(translate.get('V'))
 }
 
 testCase()
